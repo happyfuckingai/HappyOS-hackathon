a3c5d810648776851fce33de40cf1a1b2e95c4f9
Marcus Gladh <mekopeka@gmail.com>
2025-10-29 19:46:51 +0100

    Delete happyos_sdk directory

--- happyos_sdk/industries/healthcare/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Healthcare Industry Templates
-
-Provides HIPAA-compliant agent templates for healthcare industry.
-"""
-
-# Healthcare templates will be implemented in future tasks
-__all__ = []
