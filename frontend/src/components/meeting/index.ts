d69c02e2b12c22a19bd410dd72158bfea41319cf
Marcus Gladh <mekopeka@gmail.com>
2025-11-14 01:59:28 +0100

    Delete frontend directory

--- frontend/src/components/meeting/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as VideoGrid } from './VideoGrid';
-export { default as SideParticipants } from './SideParticipants';
-export { default as FabMenu } from './FabMenu';
-export { default as AIPanel } from './AIPanel';
-export { default as AISidebar } from './AISidebar';
--- frontend/src/components/meeting/tabs/index.ts
+++
@@ -1,7 +0,0 @@
-// Lazy load tab components for better code splitting
-import { lazy } from 'react';
-
-export const SettingsTab = lazy(() => import(/* webpackChunkName: "settings-tab" */ './SettingsTab'));
-export const ChatTab = lazy(() => import(/* webpackChunkName: "chat-tab" */ './ChatTab'));
-export const ShareRecordingTab = lazy(() => import(/* webpackChunkName: "share-tab" */ './ShareRecordingTab'));
-export const AITab = lazy(() => import(/* webpackChunkName: "ai-tab" */ './AITab'));
